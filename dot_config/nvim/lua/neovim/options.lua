892f01d67969ed2a2d6e1bd699b1d0b3a505a099
Shinichi Ikezawa <shinichi@ikezawa.tech>
2024-06-21 19:49:09 +0900

    Neovim用の設定を色々追加した

--- dot_config/nvim/lua/neovim/options.lua
+++ dot_config/nvim/lua/neovim/options.lua
@@ -1,3 +1,16 @@
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.clipboard = 'unnamedplus'
+vim.opt.swapfile = false
+vim.opt.termguicolors = true
+
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.autoindent = true
+
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
