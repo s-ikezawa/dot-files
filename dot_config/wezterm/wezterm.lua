69be46fb045aa459ed0a8d2daeb8d0987726ef07
Shinichi Ikezawa <shinichi@ikezawa.tech>
2024-06-21 15:17:34 +0900

    weztermのフォントとカラースキーマを設定した

---
+++ dot_config/wezterm/wezterm.lua
@@ -0,0 +1,8 @@
+local wezterm = require 'wezterm'
+local config = {}
+
+config.font = wezterm.font 'UDEV Gothic 35NF'
+config.font_size = 15.0
+config.color_scheme = "Catppuccin Macchiato"
+
+return config
