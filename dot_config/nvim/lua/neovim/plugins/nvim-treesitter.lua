914a18f01271649366b33d87831029b63b7ebed0
Shinichi Ikezawa <shinichi@ikezawa.tech>
2024-06-22 14:07:14 +0900

    Neovimにtreesitterをインストールした

---
+++ dot_config/nvim/lua/neovim/plugins/nvim-treesitter.lua
@@ -0,0 +1,18 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function()
+    local treesitter = require("nvim-treesitter.configs")
+    treesitter.setup({
+      highlight = {
+        enable = true,
+      },
+      indent = {
+        enable = true,
+      },
+      ensure_installed = {
+        "lua",
+      }
+    })
+  end
+}
