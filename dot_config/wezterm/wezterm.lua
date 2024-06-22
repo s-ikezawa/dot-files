6c486b47c5685f2dcad68c34b73cb8df52b81c71
Shinichi Ikezawa <shinichi@ikezawa.tech>
2024-06-22 13:49:32 +0900

    Weztermのフォント指定の記述方法を変更した

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,8 +1,16 @@
 local wezterm = require 'wezterm'
 local config = {}
 
-config.font = wezterm.font 'UDEV Gothic 35NF'
+config.font = wezterm.font(
+  'UDEV Gothic 35NF',
+  {
+    weight="Regular",
+    stretch="Normal",
+    italic=true
+  }
+)
 config.font_size = 15.0
 config.color_scheme = "Catppuccin Macchiato"
+config.adjust_window_size_when_changing_font_size = false
 
 return config
