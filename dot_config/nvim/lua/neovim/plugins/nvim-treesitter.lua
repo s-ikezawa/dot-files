6c39a95f51c8604125c55c3775cd3553782796cf
Shinichi Ikezawa <shinichi@ikezawa.tech>
2024-06-22 15:50:21 +0900

    選択範囲の拡大をできるように設定した

--- dot_config/nvim/lua/neovim/plugins/nvim-treesitter.lua
+++ dot_config/nvim/lua/neovim/plugins/nvim-treesitter.lua
@@ -1,5 +1,6 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  event = { "BufReadPre", "BufNewFile" },
   build = ":TSUpdate",
   config = function()
     local treesitter = require("nvim-treesitter.configs")
@@ -12,6 +13,19 @@ return {
       },
       ensure_installed = {
         "lua",
+        "vim",
+        "vimdoc",
+        "markdown",
+        "markdown_inline"
+      },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<C-space>",
+          node_incremental = "<C-space>",
+          scope_incremental = false,
+          node_decremental = "<BS>",
+        }
       }
     })
   end
