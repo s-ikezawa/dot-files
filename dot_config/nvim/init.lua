20292de256ef273678118542cd79d12429031601
Shinichi Ikezawa <shinichi@ikezawa.tech>
2024-06-22 13:50:46 +0900

    Neovimにカラースキーマをインストールした

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,5 +1,6 @@
 if vim.g.vscode then
   require("code.options")
 else
+  require("neovim.lazy")
   require("neovim.options")
 end
--- dot_config/nvim/lua/neovim/options.lua
+++ dot_config/nvim/lua/neovim/options.lua
@@ -3,6 +3,7 @@ vim.opt.relativenumber = true
 vim.opt.clipboard = 'unnamedplus'
 vim.opt.swapfile = false
 vim.opt.termguicolors = true
+vim.cmd.colorscheme "catppuccin"
 
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
