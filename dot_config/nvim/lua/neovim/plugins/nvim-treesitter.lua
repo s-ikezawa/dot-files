dfc5a30d4a2ab215e0a586e89807c5f33a164d4e
Shinichi Ikezawa <shinichi@ikezawa.tech>
2024-06-23 01:33:17 +0900

    treesitter-textobjectsとcontextをインストールした

--- dot_config/nvim/lua/neovim/plugins/nvim-treesitter.lua
+++ dot_config/nvim/lua/neovim/plugins/nvim-treesitter.lua
@@ -2,20 +2,40 @@ return {
   "nvim-treesitter/nvim-treesitter",
   dependencies = {
     "RRethy/nvim-treesitter-endwise",
+    { "nvim-treesitter/nvim-treesitter-textobjects" },
+    {
+      "nvim-treesitter/nvim-treesitter-context",
+      opts = { enable = true, mode = "topline", line_numbers = true },
+    },
   },
   event = { "BufReadPre", "BufNewFile" },
   build = ":TSUpdate",
   config = function()
     local treesitter = require("nvim-treesitter.configs")
+
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = { "markdown" },
+      callback = function(ev)
+        require("treesitter-context").disable()
+      end,
+    })
+
     treesitter.setup({
       highlight = {
         enable = true,
+        disable = { "csv" },
       },
       indent = {
         enable = true,
       },
       endwise = { -- nvim-treesitter-endwise
         enable = true,
+      },
+      textobjects = {
+        select = {
+          enable = true,
+          lookahead = true,
+        },
       },
       incremental_selection = {
         enable = true,
