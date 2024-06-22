35d748562e7a7b87125c3183ef66120561bd8f97
Shinichi Ikezawa <shinichi@ikezawa.tech>
2024-06-22 16:22:44 +0900

    Insertモード中にカーソル移動が出来るように設定した

--- dot_config/nvim/lua/neovim/keymaps.lua
+++ dot_config/nvim/lua/neovim/keymaps.lua
@@ -2,8 +2,14 @@ vim.g.mapleader = " "
 
 local keymap = vim.keymap
 
-keymap.set("i", "jj", "<ESC>", { desc = "Exit insert mode" })
-keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
+-- Insert Mode
+keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode" })
+keymap.set("i", "<C-h>", "<Left>", { desc = "Move one character to the left in insert mode" })
+keymap.set("i", "<C-j>", "<Down>", { desc = "Move down one line in insert mode" })
+keymap.set("i", "<C-k>", "<Up>", { desc = "Move up one line in insert mode" })
+keymap.set("i", "<C-l>", "<Right>", { desc = "Move one character to the right in insert mode" })
+keymap.set("i", "<C-a>", "<Home>", { desc = "Move to the beginning ot the line in insert mode" })
+keymap.set("i", "<C-e>", "<End>", { desc = "Move to end of line in insert mode" })
 
 -- FileExplorer
 keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" })
